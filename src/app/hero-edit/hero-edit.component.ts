79d81a78930de52c6454974724b5ee41264a984e
SAMPAIO1748 <nicolas.sampaio17@gmail.com>
2023-11-30 14:43:23 +0100

    edit

--- src/app/hero-edit/hero-edit.component.ts
+++ src/app/hero-edit/hero-edit.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Hero } from '../types/hero.type';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './hero-edit.component.html',
   styleUrl: './hero-edit.component.css'
 })
 export class HeroEditComponent {
 
   id;
+  hero?: Hero;
 
-  constructor(private route: ActivatedRoute){
+  constructor(private route: ActivatedRoute, private herosService: HeroService){
       this.id = route.snapshot.paramMap.get('id');
-      console.log(this.id);
+      // console.log(this.id);
+  }
+
+  ngOnInit(){
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.hero = this.herosService.getHeroById(id);
+    console.log(this.hero);
   }
 
 }
