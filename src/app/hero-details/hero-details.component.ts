9d03f6c58b8c904bcbaf4309e7c41fe81bbf25ee
SAMPAIO1748 <nicolas.sampaio17@gmail.com>
2023-11-30 14:55:14 +0100

    link

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../types/hero.type';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-hero-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './hero-details.component.html',
   styleUrl: './hero-details.component.css'
 })
